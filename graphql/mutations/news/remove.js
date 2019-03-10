8905c3f7b5234674ded0edc21dd751a7b3250c86
vinicius rodrigues <vinitecnico@gmail.com>
2019-03-09 23:45:03 -0300

    fix remove files

--- graphql/mutations/news/remove.js
+++ graphql/mutations/news/remove.js
@@ -2,6 +2,31 @@ const GraphQLNonNull = require('graphql').GraphQLNonNull;
 const GraphQLString = require('graphql').GraphQLString;
 const NewsType = require('../../types/news');
 const NewsModel = require('../../../models/news');
+const Q = require('Q');
+const fetch = require('isomorphic-fetch');
+const Dropbox = require('dropbox').Dropbox;
+const config = require('../../../config/config');
+const dbx = new Dropbox({ accessToken: config.keyDropbox, fetch: fetch });
+
+const deleteFile = (db, id) => {
+  const defer = Q.defer();
+  db.findById(id).exec()
+    .then((data) => {
+      if (data && data.fileName) {
+        dbx.filesDelete({ path: `/gecanews/${data.fileName}` })
+          .then((result) => {
+            defer.resolve(true);
+          })
+          .catch((error) => {
+            defer.resolve(false);
+          });
+      }
+      else {
+        defer.resolve(true);
+      }
+    });
+  return defer.promise;
+}
 
 exports.remove = {
   type: NewsType.newsType,
@@ -11,10 +36,13 @@ exports.remove = {
     }
   },
   resolve(root, params) {
-    const removedNews = NewsModel.findByIdAndRemove(params.id).exec();
-    if (!removedNews) {
-      throw new Error('Error')
-    }
-    return removedNews;
+    return deleteFile(NewsModel, params.id)
+      .then(() => {
+        const removedNews = NewsModel.findByIdAndRemove(params.id).exec();
+        if (!removedNews) {
+          throw new Error('Error')
+        }
+        return removedNews;
+      });
   }
 }
--- graphql/mutations/news/update.js
+++ graphql/mutations/news/update.js
@@ -5,6 +5,30 @@ const GraphQLBoolean = require('graphql').GraphQLBoolean;
 const moment = require('moment');
 const NewsType = require('../../types/news');
 const newsModel = require('../../../models/news');
+const Q = require('Q');
+const fetch = require('isomorphic-fetch');
+const Dropbox = require('dropbox').Dropbox;
+const config = require('../../../config/config');
+const dbx = new Dropbox({ accessToken: config.keyDropbox, fetch: fetch });
+
+const deleteFile = (db, id, fileName) => {
+  const defer = Q.defer();
+  db.findById(id).exec()
+    .then((data) => {
+      if (data && data.fileName && data.fileName != fileName) {
+        dbx.filesDelete({ path: `/gecanews/${data.fileName}` })
+          .then((result) => {
+            defer.resolve(true);
+          })
+          .catch((error) => {
+            defer.resolve(false);
+          });
+      } else {
+        defer.resolve(true);
+      }
+    });
+  return defer.promise;
+}
 
 exports.update = {
   type: NewsType.newsType,
@@ -33,16 +57,19 @@ exports.update = {
     }
   },
   resolve(root, params) {
-    return newsModel.findByIdAndUpdate(
-      params.id,
-      {
-        $set: {
-          title: params.title, description: params.description, imageUrl: params.imageUrl, fileName: params.fileName,
-          status: params.status, showTimeMilliseconds: params.showTimeMilliseconds, lastUpdateDate: moment()
-        }
-      },
-      { new: true }
-    )
-      .catch(err => new Error(err));
+    return deleteFile(newsModel, params.id, params.fileName)
+      .then(() => {
+        return newsModel.findByIdAndUpdate(
+          params.id,
+          {
+            $set: {
+              title: params.title, description: params.description, imageUrl: params.imageUrl, fileName: params.fileName,
+              status: params.status, showTimeMilliseconds: params.showTimeMilliseconds, lastUpdateDate: moment()
+            }
+          },
+          { new: true }
+        )
+          .catch(err => new Error(err));
+      });
   }
 }
